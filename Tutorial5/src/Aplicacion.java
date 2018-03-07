d77870101fad29a3a7c08db1a57191eaf1c9f61b
Greco <Greco>
2018-03-07 11:51:30 -0300

    Tutorial de prueba JAVA curso UDEMY

---
+++ Tutorial5/src/Aplicacion.java
@@ -0,0 +1,17 @@
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		
+		int miEntero = 15;
+		
+		if(miEntero < 10) {
+			System.out.println("Si, es verdad!");
+		}else if(miEntero > 20){
+			System.out.println("Nop, no es verdad!");
+		}else {
+			System.out.println("Ninguna de las anteriores");
+		}
+	}
+
+}
